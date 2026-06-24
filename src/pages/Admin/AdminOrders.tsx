8328ea2794f21d4ac86309e4e6c5a528ccdd34cb
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-06-24 13:56:11 +0300

    remove unused imports

--- src/pages/Admin/AdminOrders.tsx
+++ src/pages/Admin/AdminOrders.tsx
@@ -1,4 +1,4 @@
-import { Eye, MoreHorizontal, Search } from "lucide-react";
+import { Eye, MoreHorizontal } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -14,7 +14,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import {
   Sheet,
